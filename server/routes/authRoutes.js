bbe897fc1b368279eee125ed03effbdfd81ee8e4
Afonso Domingos (Vibe) <52966432+afonsoDomingos@users.noreply.github.com>
2026-07-20 15:36:56 +0200

    chore: remove temporary seed superadmin route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, registerUser, seedSuperadmin } = require('../controllers/authController');
+const { loginUser, registerUser } = require('../controllers/authController');
 
 router.post('/login', loginUser);
 router.post('/register', registerUser);
-router.post('/seed-superadmin', seedSuperadmin);
 
 module.exports = router;
